a3ccd96e2b0508a1c45e41ef390c60c6b732ad5e
dgsilva <diegorj93@gmail.com>
2022-09-19 22:30:53 -0300

    implementado a busca pelo nome

--- src/main/java/com/algawork/api/repositories/ClienteRepository.java
+++ src/main/java/com/algawork/api/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.algawork.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.algawork.api.domain.modal.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+	List<Cliente>findByNome(String nome);
 }
