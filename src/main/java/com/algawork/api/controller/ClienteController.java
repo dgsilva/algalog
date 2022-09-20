7c482995d1ed1e261bfd5dc4d7ee1cdd2be31154
dgsilva <diegorj93@gmail.com>
2022-09-19 22:32:21 -0300

    implementado a busca pelo nome e id no controller

--- src/main/java/com/algawork/api/controller/ClienteController.java
+++ src/main/java/com/algawork/api/controller/ClienteController.java
@@ -1,20 +1,25 @@
 package com.algawork.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algawork.api.domain.modal.Cliente;
 import com.algawork.api.repositories.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-	@Autowired
+	
 	private ClienteRepository clienteRepository;
 	
 	@GetMapping()
@@ -23,4 +28,20 @@ public class ClienteController {
 	return list;
 	}
 	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente>cliente = clienteRepository.findById(clienteId);
+		 
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping("/nome-por")
+	public List<Cliente>findAll(String nome){
+	List<Cliente> list =  clienteRepository.findByNome(nome);
+	return list;
+	}
 }
