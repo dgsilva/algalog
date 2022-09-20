fa2ce020c0cd06ef3741cf9893447e77130acc41
dgsilva <diegorj93@gmail.com>
2022-09-19 22:25:42 -0300

    melhorando a entidade cliente

--- src/main/java/com/algawork/api/domain/modal/Cliente.java
+++ src/main/java/com/algawork/api/domain/modal/Cliente.java
@@ -1,13 +1,23 @@
 package com.algawork.api.domain.modal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Cliente {
-
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
+	
 	private String telefone;
 	
 }
